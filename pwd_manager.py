213114d80ebc7b608b5497bc597d2221d9eae76f
riokishanea <workflow.riokishan@gmail.com>
2025-12-22 11:28:10 +0530

    Advanced View - username / password

--- pwd_manager.py
+++ pwd_manager.py
@@ -5,13 +5,14 @@ def add():
     user_pwd = input("Password: ")
 
     with open('password.txt', 'a') as f:
-        f.write(user_name + "|" + user_pwd)   
+        f.write(user_name + "|" + user_pwd + "\n")
 
 def view():
     with open('password.txt','r') as f:
         for line in f.readlines():
-            print(line)
-
+            data = line.strip()
+            u_name, u_pwd = data.split("|")
+            print("Username:",u_name + "\n" + "Password:",u_pwd)
 
 while True:
     mode = input("Would you like to add new password or view existing password? ( add / view ). Enter 'Q' to quit \n").lower()
@@ -21,6 +22,5 @@ while True:
         add()
     elif mode == "view":
         view()
-
     else:
         print("Enter a valid input.")
