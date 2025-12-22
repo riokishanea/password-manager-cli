c748e96c45e21796186f557f981ca59b716fc3cc
riokishanea <workflow.riokishan@gmail.com>
2025-12-22 16:03:39 +0530

    Added Encryption - Final commit

--- pwd_manager.py
+++ pwd_manager.py
@@ -1,21 +1,52 @@
-master_pwd = input("Enter master password: ")
+import cryptocode
+import os
+
+master_pwd = input("Enter master password to enter the vault: ")
 
 def add():
     user_name = input("Username: ")
     user_pwd = input("Password: ")
 
+    encrypted_pwd = cryptocode.encrypt(user_pwd, master_pwd)
+
     with open('password.txt', 'a') as f:
-        f.write(user_name + "|" + user_pwd + "\n")
+        f.write(user_name + "|" + encrypted_pwd + "\n")
+
+    print(f"Success! Saved password for {user_name}")
 
 def view():
-    with open('password.txt','r') as f:
-        for line in f.readlines():
-            data = line.strip()
-            u_name, u_pwd = data.split("|")
-            print("Username:",u_name + "\n" + "Password:",u_pwd)
+
+    if not os.path.exists('password.txt'):
+        print("Vault is empty, No passwords saved yet.")
+        return
+    
+    try:
+        with open('password.txt','r') as f:
+
+            lines = f.readlines()
+
+            if len(lines) == 0:
+                print("Vault is empty, Add a password first!")
+                return
+
+            for line in lines:
+                data = line.strip()
+                u_name, u_enc_pwd = data.split("|")
+
+                decoded_pwd = cryptocode.decrypt(u_enc_pwd, master_pwd)
+
+                if decoded_pwd:
+                    print("Username:",u_name + "\n" + "Password:", decoded_pwd )
+                    print()
+                else:
+                    print("Username:",u_name + "\n" + "Password: [LOCKED - Wrong key]",)
+
+    except FileNotFoundError:
+        print("No passwords saved yet!")
+
 
 while True:
-    mode = input("Would you like to add new password or view existing password? ( add / view ). Enter 'Q' to quit \n").lower()
+    mode = input("---- Vault Menu ---- \n\nAdd / View / Quit (Q) \n\nEnter a option to continue : ").lower()
     if mode == "q":
         break
     if mode == "add":
